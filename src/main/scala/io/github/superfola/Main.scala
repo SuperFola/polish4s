03e5b3027aa042881c75641aa538dec34e39c6b4
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-15 18:05:36 +0200

    adds a working command line runner

--- src/main/scala/io/github/superfola/Main.scala
+++ src/main/scala/io/github/superfola/Main.scala
@@ -1,15 +1,29 @@
 package io.github.superfola
 
 import cats.effect.{ExitCode, IO, IOApp}
-import io.github.superfola.types._
+import io.github.superfola.types.{Leaf, Node, Tree}
+
+import scala.util.chaining.scalaUtilChainingOps
 
 object Main extends IOApp {
-  override def run(args: List[String]): IO[ExitCode] = {
-    if (args.length == 0) {
+  def reduceToEnd(tree: Tree, depth: Int = 0): IO[Unit] =
+    tree match {
+      case a: Node =>
+        IO(println(s"$depth) $a")) *> reduceToEnd(a.reduce, depth + 1)
+      case Leaf(value) => IO(println(s"$depth) $value"))
+    }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    if (args.isEmpty) {
       IO(println("Not enough arguments")) *> IO.pure(ExitCode.Error)
+    } else {
+      Parser(args.head) match {
+        case Some(value) =>
+          IO {
+            println(s"Got ${args.head}\nInfix notation: ${value}\nPrefix notation: ${value.toPrefixNotation}")
+            println(s"Computed value: ${value.computed}")
+          } *> reduceToEnd(value) *> IO.pure(ExitCode.Success)
+        case None => IO(println("Error: couldn't parse given expression")) *> IO.pure(ExitCode.Error)
+      }
     }
-    else {
-      IO(println(args(0))) *> IO.pure(ExitCode.Success)
-    }
-  }
 }
