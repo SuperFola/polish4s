7eaf2e39c356020e566533785ec0958b2aaf68ac
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-15 13:49:02 +0200

    enhances the binary tree definition

---
+++ src/main/scala/io/github/superfola/Tree.scala
@@ -0,0 +1,27 @@
+package io.github.superfola
+
+import math.Numeric
+import math.Numeric.Implicits._
+
+sealed trait Tree[A] {
+  def computed: A
+  def reduce: Tree[A]
+}
+
+final case class Leaf[A: Numeric](value: A) extends Tree[A] {
+  override def computed: A = value
+
+  override def reduce: Tree[A] = this
+
+  override def toString: String = value.toString
+}
+
+final case class Operator[A: Numeric](name: String, functor: (A, A) => A)
+
+final case class Node[A: Numeric](op: Operator[A], left: Tree[A], right: Tree[A]) extends Tree[A] {
+  override def computed: A = op.functor(left.computed, right.computed)
+
+  override def reduce: Tree[A] = Node[A](op, left.reduce, right.reduce)
+
+  override def toString: String = s"($left ${op.name} $right)"
+}
