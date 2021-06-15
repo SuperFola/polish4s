d6b1687ae3c63c526840abd5e4665bfaafd1ca14
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-15 14:44:26 +0200

    adds more tests

--- src/test/scala/io/github/superfola/TypesSpec.scala
+++ src/test/scala/io/github/superfola/TypesSpec.scala
@@ -1,19 +1,41 @@
 package io.github.superfola
 
-import org.scalatest._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 import io.github.superfola.types._
 
 class TypesSpec extends AnyFlatSpec with Matchers {
+  val simpleTree: Tree[Int] = Leaf[Int](12)
+  val bigTree: Tree[Int] = Node(
+    Operators.add,
+    Node(
+      Operators.sub,
+      Leaf(4),
+      Leaf(3)
+    ),
+    Node(
+      Operators.mul,
+      Leaf(3),
+      Leaf(2)
+    )
+  )
+
   "A simple tree" should "hold a node" in {
-    val simpleTree: Tree[Int] = Leaf[Int](12)
     simpleTree.reduce should be(simpleTree)
     simpleTree.computed should be(12)
   }
   it should "have a depth of one" in {
-    val simpleTree: Tree[Int] = Leaf[Int](12)
     simpleTree.depth should be(1)
+  }
+
+  "A bigger tree" should "hold many nodes" in {
+    bigTree.depth should be(3)
+  }
+  it should "be evaluated to ((4 - 3) + (3 * 2))" in {
+    bigTree.toString should be("((4 - 3) + (3 * 2))")
+  }
+  it should "have a value of 7" in {
+    bigTree.computed should be(7)
   }
 }
