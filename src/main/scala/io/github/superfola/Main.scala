c60e537ee7fef23c37eb1055b3b1f8d0ef7a8528
Alexandre Plateau <lexplt.dev@gmail.com>
2021-06-15 13:49:34 +0200

    adds an example to use it

--- src/main/scala/io/github/superfola/Main.scala
+++ src/main/scala/io/github/superfola/Main.scala
@@ -1,5 +1,19 @@
 package io.github.superfola
 
-object Main {
+object Main extends App {
+  def create: Tree[Int] =
+    Node[Int](
+      Operators.add,
+      left=Node(
+        Operators.sub,
+        Leaf(1),
+        Leaf(4)
+      ),
+      right=Leaf(2)
+    )
 
+  val tree: Tree[Int] = create
+
+  println(tree)
+  println(tree.computed)
 }
